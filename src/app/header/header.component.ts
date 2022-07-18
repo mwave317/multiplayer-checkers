24fe4b0062ee4754c484692f2df845ee70b22d40
kencoppola <ken@iplaycornhole.com>
2022-07-18 17:41:36 -0400

    Wired up the buttons.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -28,7 +28,6 @@ export class HeaderComponent implements OnInit {
     //     this.player2Active = true;
     //   }
     // })
-    setTimeout(() => { this.onEndGame() }, 3000);
   }
 
   onEndGame() {
---
+++ src/app/user-options/user-options.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/services/shared.service';
+
+@Component({
+  selector: 'app-user-options',
+  templateUrl: './user-options.component.html',
+  styleUrls: ['./user-options.component.css']
+})
+export class UserOptionsComponent implements OnInit {
+  disabled: boolean = false;
+  player1Active: boolean = false;
+  player2Active: boolean = false;
+
+  constructor(private sharedService: SharedService) { }
+
+  ngOnInit(): void {
+  }
+
+  onStartGame() {
+    this.disabled = true;
+    this.player1Active = true;
+    this.sharedService.player1Active.next(this.player1Active);
+  }
+
+  onStopGame() {
+    this.disabled = false;
+    this.player1Active = false;
+    this.sharedService.player1Active.next(this.player1Active);
+  }
+
+}
